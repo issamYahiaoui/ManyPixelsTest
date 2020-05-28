d2e53bfd3b1d324ced775d67559c2e50d39cb758
issamYahiaoui <ei_yahiaoui@esi.dz>
2020-05-28 14:12:10 +0100

    Updates

---
+++ precache-manifest.f208174f6f4874a35459030b1be9c134.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "daf06c31baf658b0bf3b86f2da10786c",
+    "url": "/ManyPixelsTest/index.html"
+  },
+  {
+    "revision": "fffa541109ff37731443",
+    "url": "/ManyPixelsTest/static/css/2.63b65d4e.chunk.css"
+  },
+  {
+    "revision": "b88d300f9cb417361ee7",
+    "url": "/ManyPixelsTest/static/css/main.4c48667a.chunk.css"
+  },
+  {
+    "revision": "fffa541109ff37731443",
+    "url": "/ManyPixelsTest/static/js/2.93db8239.chunk.js"
+  },
+  {
+    "revision": "b5901214077ea32a7534a1d4eff7c84a",
+    "url": "/ManyPixelsTest/static/js/2.93db8239.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b88d300f9cb417361ee7",
+    "url": "/ManyPixelsTest/static/js/main.cd586f84.chunk.js"
+  },
+  {
+    "revision": "9db9dcbe97d016e0b233",
+    "url": "/ManyPixelsTest/static/js/runtime-main.e8bf186d.js"
+  }
+]);
